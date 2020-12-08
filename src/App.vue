4f5d39c6832ec35deba546dc1f42dbc3585f12a3
marchalscott <marchalscott@gmail.com>
2020-12-07 20:02:49 -0700

    removing the default template

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <LandingPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
