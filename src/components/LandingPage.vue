01ce0ebd20d9002802f1a4a76228263e394ee166
marchalscott <marchalscott@gmail.com>
2020-12-07 20:44:09 -0700

    working on the menu

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,10 +4,16 @@
         <a href = "" target = "_blank"> Home </a>
     </div>
     <div class="menu-item">
-        Work Examples
+        <a href = "" target = "_blank"> Work Examples </a>
     </div>
     <div class="menu-item">
-        About
+        <a href = "" target = "_blank"> Portfolio </a>
+    </div>
+    <div class="menu-item">
+        <a href = "" target = "_blank"> Social </a>
+    </div>
+    <div class="menu-item">
+        <a href = "" target = "_blank"> About </a>
     </div>
   </div>
 </template>
@@ -23,10 +29,14 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-around;
+    text-decoration: none;
 }
 
 .menu-item {
+    color: black;
     flex: 1;
+    font-family: -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, Helvetica, sans-serif;
     text-align: center;
+    text-decoration: none;
 }
 </style>
